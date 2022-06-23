fb4391eccab38333fa1d202fea1f8f45eddd66cd
Nancy <ncyarslan@gmail.com>
2022-06-23 21:48:07 +0100

    never last commit

--- src/group5/Nancy.java
+++ src/group5/Nancy.java
@@ -3,6 +3,6 @@ package group5;
 public class Nancy {
     public static void main(String[] args) {
         System.out.println("Wooden Spoon");
-        System.out.println("");
+        System.out.println("world");
     }
 }
