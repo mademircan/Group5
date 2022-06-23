21b7d5c06a626f48c13a4e63e67180c69467454e
Nancy <ncyarslan@gmail.com>
2022-06-23 21:59:11 +0100

    test1

--- src/group5/Nancy.java
+++ src/group5/Nancy.java
@@ -4,5 +4,6 @@ public class Nancy {
     public static void main(String[] args) {
         System.out.println("Wooden Spoon");
         System.out.println("world");
+        System.out.println("no cry");
     }
 }
